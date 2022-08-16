bdc8d797d3aa33335632ed36063642fa4164e2bb
ayyubnurumi <ayyubinvestment@gmail.com>
2022-08-16 14:16:37 +0700

    belajar net.js

--- pages/api/users/authenticate.js
+++ pages/api/users/authenticate.js
@@ -6,7 +6,7 @@ import { apiHandler } from '.';
 const { serverRuntimeConfig } = getConfig();
 
 // users in JSON file for simplicity, store in a db for production applications
-const users = require('data/users.json');
+const users = require('/data/users.json');
 
 export default apiHandler(handler);
 
